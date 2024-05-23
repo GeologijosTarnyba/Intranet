cfca0709073037998134ec6b8b51252e50f2baae
Šarūnas <sarunas1991@gmail.com>
2024-05-23 09:32:46 +0300

    Sukurtas .vcf (vcard) failo generavimas kontaktams

---
+++ src/LGT.Intranet/Components/In/Kontaktai/VCard.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace LGT.Intranet.Components.In.Kontaktai;
+
+public static class VCard
+{
+  public static async Task<string> vCardText(LGT.Kontaktai.KontaktaiContext db, string orgId)
+  {
+    var now = DateTime.Now;
+    var xs = await db.Set<LGT.Kontaktai.Models.Darbuotojas>()
+    .Where(uoj => uoj.DarbovieteId == orgId)
+    .Where(uoj => uoj.Until == null || uoj.Until > now)
+    .Select(uoj => new {
+      ORG = uoj.Darboviete!.Name,
+      Title = uoj.Lytis == LGT.Kontaktai.Models.Lytys.Moteris ? uoj.Etatas.Name.Vardininke : uoj.Etatas.Name.Vardininkas,
+      FN = uoj.FullName,
+      Cell = uoj.Tel1,
+      Voice = uoj.Tel2,
+      Email = uoj.Email,
+    })
+    .ToListAsync();
+    return string.Join("\n", xs.Select(uoj =>
+      $"BEGIN:VCARD\nVERSION:2.1\n{X("ORG", uoj.ORG)}{X("TITLE", uoj.Title)}{X("FN", uoj.FN)}{X("TEL;cell", uoj.Cell )}{X("TEL;voice", uoj.Voice)}{X("EMAIL;work", uoj.Email)}END:VCARD"
+    ))
+      ;
+  }
+  private static string? X(string key, string? value)
+  {
+    if(value == null)
+      return null;
+    return $"{key}:{value}\n";
+  }
+}
